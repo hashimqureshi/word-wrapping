0f0624ecd032928ec26d3d596fa571719ecfad48
ITRON\hqureshi <hashim.qureshi@itron.com>
2018-09-21 14:58:20 +0200

    removed bugs in code by creating empty list to add right words into the list

--- word processor example practice.py
+++ word processor example practice.py
@@ -1,15 +1,20 @@
 def wordWrapping(string, width):
-
-    text = string.split()
-    for word in text:
-        if len(word) + len(text[text.index(word)-len(text)+1]) <= width:
-            print(word + " " +text[text.index(word)-len(text)+1])
-        elif len(word) > width:
-            for numberOfCharacters in range(0, len(word), width):
-                print(word[numberOfCharacters:numberOfCharacters+width])
-        elif len(word) == width:
-            print(word)
-
-
-
-wordWrapping("Superlongword and is longword longerword otherword is shorter", 8)
+    lines = []
+    for text in string.split('\n'):
+        line = []
+        len_line = 0
+        for word in text.split(' '):
+            len_word = len(word)
+            if len_line + len_word <= width:
+                line.append(word)
+                len_line += len_word + 1
+            elif len(word) > width:
+                for numberOfCharacters in range(0, len(word), width):
+                    print(word[numberOfCharacters:numberOfCharacters+width])
+            else:
+                lines.append(' '.join(line))
+                line = [word]
+                len_line = len_word + 1
+        lines.append(' '.join(line))
+    return '\n'.join(lines)
+print(wordWrapping("Superlongword and is longword longerword otherword is shorter", 8))
